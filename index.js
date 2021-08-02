54eb31aad91ac65305b8b7d6ff06a24624596cad
Jaehun Kim <kkaa81@naver.com>
2021-08-02 17:32:33 +0900

    feat(backend): updata backend

--- index.js
+++ index.js
@@ -1,8 +1,85 @@
 const express = require('express') //express를 설치했기 때문에 가져올 수 있다.
-const app = express()
+const cookieparser = require('cookie-parser');
+const app = express();
+require("dotenv").config();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const port = process.env.PORT;
 
-app.listen(5000)
+const vocabulary = [];
+const user = [];
+
+app.use(express.json());
+app.use(express.static('build'));
+app.use(express.urlencoded());
+app.use(cookieparser());
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
+});
+
+app.get('/', function(req, res, next) {
+  res.send('hello')
+});
+
+app.route('/voca')
+  .get((req, res) => {
+    res.send(vocabulary);
+  })
+  .post((req, res) => {
+    const { user_id, voca } =req.body;
+    let userVoca = [];
+
+    if (!voca) {
+      for (let i = 0; i < vocabulary.length; i += 1) {
+        if (vocabulary[i]['user_id'] === user_id) {
+          userVoca.push(vocabulary[i]);
+        }
+      }
+      res.send(userVoca);
+      return;
+    }
+
+    vocabulary.push(req.body);
+    res.send(req.body);
+  })
+
+app.route('/user')
+  .get((_, res) => {
+    res.send(user);
+  })
+  .post((req, res) => {
+    user.push(req.body);
+    res.send(req.body);
+  })
+
+app.route('/login')
+  .get((_, res) => {
+    res.clearCookie('user');
+    res.send('로그아웃');
+  })
+  .post((req, res) => {
+    const { code, password } = req.body;
+    console.log(req.cookies['user']);
+    console.log(req.cookies);
+
+    if(req.cookies["user"]){
+        res.send(true);
+        return;
+    } 
+    if (!code) {
+      res.send(false);
+      return;
+    }
+    for (let i = 0; i < user.length; i += 1) {
+      if (user[i]['code'] === code && user[i]['password'] === password) {
+        const token = { userId : i };
+        res.cookie('user', i);
+        res.send(token);
+        return;
+      } 
+    }
+    res.status(404).send('로그인 실패');  
+  })
+
+app.listen(port, () => console.log("Listening on", port));
